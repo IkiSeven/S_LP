0d248a4157ca7ab0572084ed511ccaa954eac6d3
Iki <146895674+IkiSeven@users.noreply.github.com>
2024-10-17 20:50:31 -0300

    v0.01
    Primeira versão, ainda em desenvolvimento.
    40%

---
+++ main.js
@@ -0,0 +1,12 @@
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('.anim', {delay:50});
+
+const contactForm = document.getElementById('contactForm');
+const email = document.getElementById('email');
+const name = document.getElementById('name');
+const menssage = document.getElementById('menssage');
+
+// contactForm.addEventListener('submit', (e)=> {
+//     e.preventDefault();
+//     window.location.href = '';
+// });
